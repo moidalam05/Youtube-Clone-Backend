03748ee0cdba0fa65e118b9176427ced3eb210ea
moidalam05 <moidalam005@hotmail.com>
2025-07-16 19:03:27 +0530

    add cookie-parser and cors middleware; implement async and error handling utilities

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import dotenv from "dotenv";
 import connectDB from "./db/DB_Connect.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./.env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 5000, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Database connection failed:", error);
+    process.exit(1);
+  });
