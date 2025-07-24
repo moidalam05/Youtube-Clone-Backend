4ccebba9ce58a8afd51f476c0df2fd755216b015
moidalam05 <moidalam005@hotmail.com>
2025-07-24 17:16:35 +0530

    refactor user routes; add endpoints for changing password, retrieving current user, updating details, and managing avatar/cover image

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
 import {
+  changeCurrentPassword,
+  getCurrentUser,
+  getUserChannelProfile,
+  getWatchHistory,
   loginUser,
   logoutUser,
   refreshAccessToken,
   registerUser,
+  updateUserAvatar,
+  updateUserCoverImage,
+  updateUserDetails,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyUser } from "../middlewares/auth.middleware.js";
@@ -27,5 +34,19 @@ router.route("/register").post(
 router.route("/login").post(loginUser);
 router.route("/logout").post(verifyUser, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyUser, changeCurrentPassword);
+router.route("/current-user").get(verifyUser, getCurrentUser);
+router.route("/update-details").patch(verifyUser, updateUserDetails);
+
+router
+  .route("/update-avatar")
+  .patch(verifyUser, upload.single("avatar"), updateUserAvatar);
+
+router
+  .route("/update-cover-image")
+  .patch(verifyUser, upload.single("coverImage"), updateUserCoverImage);
+
+router.route("/c/:username").get(verifyUser, getUserChannelProfile);
+router.route("/watch-history").get(verifyUser, getWatchHistory);
 
 export default router;
